e1ae2d91b39b87a3b981afec417cfa91c8159516
Mrtelegrambot <148042637+Mrtelegrambot@users.noreply.github.com>
2024-04-26 22:41:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "21102617")
     API_HASH  = os.environ.get("API_HASH", "31de0d24a6b8048c48730bc420f4b70c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7123788891:AAEOzRzW7aVzwdIXSLuDHaLgJUtctCbIYGQ") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","Cluster0")     
