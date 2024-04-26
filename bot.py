fb5527b399a7d94e2064c5a4033eb2e1f2236a89
Mrtelegrambot <148042637+Mrtelegrambot@users.noreply.github.com>
2024-04-26 12:57:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from pytz import timezone
 from pyrogram import Client
 from pyrogram.raw.all import layer
-from plugins import setup.py
 from config import Config
 from aiohttp import web
 from route import web_server
